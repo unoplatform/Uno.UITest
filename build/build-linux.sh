5197690dd1a1f629fc6d4c20d965a09ec1696a70
Jerome Laban <jerome.laban@nventive.com>
2019-06-17 14:45:14 -0400

    Uno update

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -10,11 +10,11 @@ cd /uno.uitest/build
 
 npm i chromedriver@74.0.0
 npm i puppeteer@1.13.0
-mono build/nuget.exe install NUnit.ConsoleRunner -Version 3.10.0
+mono nuget.exe install NUnit.ConsoleRunner -Version 3.10.0
 
 export UNO_UITEST_TARGETURI=http://localhost:8000
 export UNO_UITEST_DRIVERPATH_CHROME=/uno.uitest/build/node_modules/chromedriver/lib/chromedriver/chromedriver
 export UNO_UITEST_CHROME_BINARY_PATH=/uno.uitest/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux
 cp /uno.uitest/build/node_modules/chromedriver/lib/chromedriver/chromedriver /uno.uitest/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux/
 
-mono NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe /uno.uitest/src/Sample/Sample.UITests/bin/Release/net47/Sample.UITests.dll
+mono /uno.uitest/build/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe /uno.uitest/src/Sample/Sample.UITests/bin/Release/net47/Sample.UITests.dll
