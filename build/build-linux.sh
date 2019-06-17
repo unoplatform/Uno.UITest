2e6a8b889b227143e9c4de4c7b3197f1a3cb3399
Jerome Laban <jerome.laban@nventive.com>
2019-06-17 14:50:41 -0400

    Adjust build

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -3,8 +3,6 @@
 msbuild /r /p:Configuration=Release /uno.uitest/src/Sample/Sample.UITests/Sample.UITests.csproj
 msbuild /r /p:Configuration=Release /uno.uitest/src/Sample/Sample.Wasm/Sample.Wasm.csproj
 
-cd /uno.uitest/src/Sample/Sample.Wasm/bin/Release/netstandard2.0/dist
-python server.py &
 
 cd /uno.uitest/build
 
@@ -16,5 +14,8 @@ export UNO_UITEST_TARGETURI=http://localhost:8000
 export UNO_UITEST_DRIVERPATH_CHROME=/uno.uitest/build/node_modules/chromedriver/lib/chromedriver/chromedriver
 export UNO_UITEST_CHROME_BINARY_PATH=/uno.uitest/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux
 cp /uno.uitest/build/node_modules/chromedriver/lib/chromedriver/chromedriver /uno.uitest/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux/
+
+cd /uno.uitest/src/Sample/Sample.Wasm/bin/Release/netstandard2.0/dist
+python server.py &
 
 mono /uno.uitest/build/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe /uno.uitest/src/Sample/Sample.UITests/bin/Release/net47/Sample.UITests.dll
