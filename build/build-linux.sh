8315bb910b51d557b1aa347ba8fe2ac220eed6f2
Jerome Laban <jerome.laban@nventive.com>
2019-06-17 15:24:48 -0400

    Adjust path

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -2,21 +2,22 @@
 
 cd /uno.uitest
 
-msbuild /r /p:Configuration=Release src/Sample/Sample.UITests/Sample.UITests.csproj
-msbuild /r /p:Configuration=Release src/Sample/Sample.Wasm/Sample.Wasm.csproj
+msbuild /r /p:Configuration=Release $BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/Sample.UITests.csproj
+msbuild /r /p:Configuration=Release $BUILD_SOURCESDIRECTORY/src/Sample/Sample.Wasm/Sample.Wasm.csproj
 
-cd /uno.uitest/build
+cd $BUILD_SOURCESDIRECTORY/build
 
 npm i chromedriver@74.0.0
 npm i puppeteer@1.13.0
 mono nuget.exe install NUnit.ConsoleRunner -Version 3.10.0
 
 export UNO_UITEST_TARGETURI=http://localhost:8000
-export UNO_UITEST_DRIVERPATH_CHROME=/uno.uitest/build/node_modules/chromedriver/lib/chromedriver/chromedriver
-export UNO_UITEST_CHROME_BINARY_PATH=/uno.uitest/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux
-cp /uno.uitest/build/node_modules/chromedriver/lib/chromedriver/chromedriver /uno.uitest/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux/
+export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver/chromedriver
+export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux
+cp $BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver/chromedriver $BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux/
 
-cd /uno.uitest/src/Sample/Sample.Wasm/bin/Release/netstandard2.0/dist
+cd $BUILD_SOURCESDIRECTORY/src/Sample/Sample.Wasm/bin/Release/netstandard2.0/dist
 python server.py &
+disown
 
-mono /uno.uitest/build/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe /uno.uitest/src/Sample/Sample.UITests/bin/Release/net47/Sample.UITests.dll
+mono $BUILD_SOURCESDIRECTORY/build/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe $BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/bin/Release/net47/Sample.UITests.dll
