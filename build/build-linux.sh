388d86f3d47c358fe6fc6be1c6c5ed033008dd61
Jerome Laban <jerome.laban@nventive.com>
2019-06-17 21:46:24 -0400

    Adjust screenshots output

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -14,7 +14,7 @@ mono nuget.exe install NUnit.ConsoleRunner -Version 3.10.0
 export UNO_UITEST_TARGETURI=http://localhost:8000
 export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver
 export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux/chrome
-export UNO_UITEST_SCREENSHOTS_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots
+export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots
 
 ## The python server serves the current working directory, and may be changed by the nunit runner
 bash -c "cd $BUILD_SOURCESDIRECTORY/src/Sample/Sample.Wasm/bin/Release/netstandard2.0/dist/; python server.py &"
