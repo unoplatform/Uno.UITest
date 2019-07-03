a3199df0ecf311e08f4d76dace57cf48b105e3be
Jerome Laban <jerome.laban@nventive.com>
2019-07-02 21:49:42 -0400

    Remove java/android overrides

--- src/Sample/Sample.UITests/AppInitializer.cs
+++ src/Sample/Sample.UITests/AppInitializer.cs
@@ -65,8 +65,10 @@ namespace SamplesApp.UITests
 
 		private static IApp CreateAndroidApp(bool alreadyRunningApp)
 		{
-			Environment.SetEnvironmentVariable("ANDROID_HOME", @"C:\Program Files (x86)\Android\android-sdk");
-			Environment.SetEnvironmentVariable("JAVA_HOME", @"C:\Program Files\Android\Jdk\microsoft_dist_openjdk_1.8.0.25");
+			// To set in case of Xamarin.UITest errors
+			//
+			// Environment.SetEnvironmentVariable("ANDROID_HOME", @"C:\Program Files (x86)\Android\android-sdk");
+			// Environment.SetEnvironmentVariable("JAVA_HOME", @"C:\Program Files\Android\Jdk\microsoft_dist_openjdk_1.8.0.25");
 
 			var androidConfig = Xamarin.UITest.ConfigureApp
 				.Android
