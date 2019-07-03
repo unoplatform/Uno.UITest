276b84cfabdcad794778335cc9bbca81b1b25fce
Jerome Laban <jerome.laban@nventive.com>
2019-07-02 22:48:29 -0400

    Move the one-time initialization

--- src/Sample/Sample.UITests/TestBase.cs
+++ src/Sample/Sample.UITests/TestBase.cs
@@ -4,23 +4,17 @@ using SamplesApp.UITests;
 using Uno.UITest;
 using Uno.UITest.Selenium;
 
-[TestFixture]
-public class AssemblySetup
-{
-
-	[OneTimeSetUpAttribute]
-	public void Setup()
-	{
-		// Start the app only once, so the tests runs don't restart it
-		// and gain some time for the tests.
-		AppInitializer.StartApp(alreadyRunningApp: false);
-	}
-}
-
 namespace Sample.UITests
 {
 	public class TestBase
 	{
+		static TestBase()
+		{
+			// Start the app only once, so the tests runs don't restart it
+			// and gain some time for the tests.
+			AppInitializer.StartApp(alreadyRunningApp: false);
+		}
+
 		protected IApp App { get; private set; }
 
 		[SetUp]
