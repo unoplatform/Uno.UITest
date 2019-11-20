8cf9a186b1472f218dde4e4dccb02b41b8d99d08
David Oliver <david.oliver@nventive.com>
2019-11-20 11:42:40 -0500

    [Xamarin] Wire up app.Scroll() methods

---
+++ src/Sample/Sample.UITests/Scroll_Tests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers.Queries;
+using Query = System.Func<Uno.UITest.IAppQuery, Uno.UITest.IAppQuery>;
+
+
+namespace Sample.UITests
+{
+	public class Scroll_Tests : TestBase
+	{
+		[Test]
+		public void When_ScrollDownTo()
+		{
+			Query testSelector = q => q.Marked("ScrollViewer01");
+
+			App.WaitForElement(testSelector);
+			App.Tap(testSelector);
+
+			Query sv = q => q.Marked("TargetScrollViewer");
+
+			App.WaitForElement(sv);
+
+			Query scp = q => q.Marked("TargetScrollViewer")
+				.Descendant(className: "ScrollContentPresenter");
+
+			App.WaitForElement(scp);
+
+			Query resultTextBlock = q => q.Marked("ResultTextBlock");
+
+			App.ScrollDownTo(
+				resultTextBlock,
+				sv
+			);
+
+			Query button = q => q.Marked("TestButton");
+			App.Tap(button);
+			App.WaitForDependencyPropertyValue(resultTextBlock, "Text", "Success");
+
+		}
+		[Test]
+		public void When_ScrollDown()
+		{
+			Query testSelector = q => q.Marked("ScrollViewer01");
+
+			App.WaitForElement(testSelector);
+			App.Tap(testSelector);
+
+			Query sv = q => q.Marked("TargetScrollViewer");
+
+			App.WaitForElement(sv);
+
+			for (var i = 0; i < 3; i++)
+			{
+				App.ScrollDown(sv);
+			}
+
+			Query resultTextBlock = q => q.Marked("ResultTextBlock");
+
+			Query button = q => q.Marked("TestButton");
+			App.Tap(button);
+			App.WaitForDependencyPropertyValue(resultTextBlock, "Text", "Success");
+		}
+	}
+}
---
+++ src/Uno.UITest.Xamarin/Extensions/ScrollStrategyExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.UITest.Xamarin.Extensions
+{
+	public static class ScrollStrategyExtensions
+	{
+		public static global::Xamarin.UITest.ScrollStrategy ToXamarinScrollStrategy(this Uno.UITest.ScrollStrategy unoScrollStrategy)
+		{
+			switch (unoScrollStrategy)
+			{
+				case ScrollStrategy.Auto:
+					return global::Xamarin.UITest.ScrollStrategy.Auto;
+				case ScrollStrategy.Gesture:
+					return global::Xamarin.UITest.ScrollStrategy.Gesture;
+				case ScrollStrategy.Programmatically:
+					return global::Xamarin.UITest.ScrollStrategy.Programmatically;
+			}
+
+			throw new ArgumentOutOfRangeException(nameof(unoScrollStrategy));
+		}
+	}
+}
