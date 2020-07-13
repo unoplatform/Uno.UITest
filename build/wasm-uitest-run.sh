638b29443495bda1c4a21d08a6d7318d996b86b9
Jerome Laban <jerome.laban@nventive.com>
2020-07-12 21:31:41 -0400

    ci: Update chrome for wasm tests

--- build/wasm-uitest-run.sh
+++ build/wasm-uitest-run.sh
@@ -7,13 +7,13 @@ msbuild /r /p:Configuration=Release $BUILD_SOURCESDIRECTORY/src/Sample/Sample.Wa
 
 cd $BUILD_SOURCESDIRECTORY/build
 
-npm i chromedriver@74.0.0
-npm i puppeteer@1.13.0
+npm i chromedriver@83.0.1
+npm i puppeteer@3.1.0
 mono nuget.exe install NUnit.ConsoleRunner -Version 3.10.0
 
 export UNO_UITEST_TARGETURI=http://localhost:8000
 export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver
-export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux/chrome
+export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-756035/chrome-linux/chrome
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wasm
 export UNO_UITEST_PLATFORM=Browser
 export UNO_UITEST_CHROME_CONTAINER_MODE=true
