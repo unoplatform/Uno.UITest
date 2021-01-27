5eac5ed355b4b6f8ec61645a1c3d4fd447dee1d3
Jerome Laban <jerome.laban@nventive.com>
2021-01-27 11:12:14 -0500

    chore: Update to uno latest

--- build/android-uitest-run.sh
+++ build/android-uitest-run.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+
+## Adjust those variables for your project
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/android
-export UNO_UITEST_PLATFORM=Android
 export UNO_UITEST_ANDROIDAPK_PATH=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.Droid/bin/Release/uno.platform.uitestsample.apk
 export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/Sample.UITests.csproj
 export UNO_UITEST_ANDROID_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.Droid/Sample.Droid.csproj
 export UNO_UITEST_BINARY=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/bin/Release/net47/Sample.UITests.dll
-export UNO_UITEST_NUNIT_VERSION=3.10.0
 export UNO_UITEST_ANDROID_API_LEVEL=28
+
+## Less commonly modified variables
+export UNO_UITEST_PLATFORM=Android
+export UNO_UITEST_NUNIT_VERSION=3.10.0
 export UNO_UITEST_NUGET_URL=https://dist.nuget.org/win-x86-commandline/v5.7.0/nuget.exe
 
 cd $BUILD_SOURCESDIRECTORY/build
--- build/ios-uitest-run.sh
+++ build/ios-uitest-run.sh
@@ -2,12 +2,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-export UNO_UITEST_PLATFORM=iOS
+## Adjust those variables for your project
 export UNO_UITEST_IOSBUNDLE_PATH=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.iOS/bin/iPhoneSimulator/Release/Sample.app
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/ios
 export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/Sample.UITests.csproj
 export UNO_UITEST_BINARY=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/bin/Release/net47/Sample.UITests.dll
 export UNO_UITEST_IOS_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.iOS/Sample.iOS.csproj
+
+## Less commonly modified variables
+export UNO_UITEST_PLATFORM=iOS
 export UNO_UITEST_NUNIT_VERSION=3.10.0
 export UNO_UITEST_NUGET_URL=https://dist.nuget.org/win-x86-commandline/v5.7.0/nuget.exe
 
