b9896ef03e338279c393e4f947049a0b7342a01a
Guillaume Eniona <guillaume.eniona@nventive.com>
2024-05-03 09:55:53 -0400

    Update to vs test 17.9.X to fix JAVA_HOME environement not found

--- src/Sample/Sample.UITests/SetPropertyValue_Tests.cs
+++ src/Sample/Sample.UITests/SetPropertyValue_Tests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using Uno.UITest.Helpers.Queries;
 using Query = System.Func<Uno.UITest.IAppQuery, Uno.UITest.IAppQuery>;
 
@@ -24,16 +25,16 @@ namespace Sample.UITests
 			App.WaitForElement(tb2);
 
 			var tb1Value = App.Query(q => tb1(q).GetDependencyPropertyValue("Text").Value<string>()).First();
-			Assert.AreEqual("None", tb1Value);
+			ClassicAssert.AreEqual("None", tb1Value);
 
 			var tb2Value = App.Query(q => tb2(q).GetDependencyPropertyValue("Text").Value<string>()).First();
-			Assert.AreEqual("None", tb2Value);
+			ClassicAssert.AreEqual("None", tb2Value);
 
 			var assignedValue = App.Query(q => tb1(q).SetDependencyPropertyValue("Text", "test value").Value<string>()).First();
 			tb2Value = App.Query(q => tb2(q).GetDependencyPropertyValue("Text").Value<string>()).First();
 
-			Assert.AreEqual("test value", assignedValue);
-			Assert.AreEqual("test value", tb2Value);
+			ClassicAssert.AreEqual("test value", assignedValue);
+			ClassicAssert.AreEqual("test value", tb2Value);
 		}
 	}
 }
