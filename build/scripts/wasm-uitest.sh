a5b0933de0597bf0f78566d240c57485dd02bfad
Jerome Laban <jerome@platform.uno>
2025-03-05 23:30:04 -0500

    chore: Adjust wasm tests

--- build/scripts/wasm-uitest.sh
+++ build/scripts/wasm-uitest.sh
@@ -7,11 +7,14 @@ dotnet build -c Release -f net8.0-browserwasm -p:TargetFrameworks=net8.0-browser
 # Start the server
 dotnet run --project $UNO_UITEST_WASM_PROJECT -f net8.0-browserwasm -c Release --no-build &
 
-npm i chromedriver@$UNO_UITEST_PUPETTEER_VERSION
-npm i puppeteer@$UNO_UITEST_CHROMEDRIVER_VERSION
+npm i chromedriver@$UNO_UITEST_CHROMEDRIVER_VERSION
+npm i puppeteer@$UNO_UITEST_PUPETTEER_VERSION
 wget $UNO_UITEST_NUGET_URL
 mono nuget.exe install NUnit.ConsoleRunner -Version $UNO_UITEST_NUNIT_VERSION
 
 mkdir -p $UNO_UITEST_SCREENSHOT_PATH
 
-dotnet test -c Release $UNO_UITEST_PROJECT || true
+dotnet test -c Release \
+	$UNO_UITEST_PROJECT \
+	--logger "nunit;LogFileName=$UNO_TEST_RESULTS_FILE" \
+	|| true
