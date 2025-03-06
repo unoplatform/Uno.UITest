269b38b74e8c6b802fa0e2b26a20f229c4d29cb8
Jerome Laban <jerome@platform.uno>
2025-03-06 11:17:00 -0500

    chore: Adjust android apk path, bump xamarin.uitest

--- build/android-uitest-run.sh
+++ build/android-uitest-run.sh
@@ -2,7 +2,7 @@
 
 ## Adjust those variables for your project
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/android
-export UNO_UITEST_ANDROIDAPK_PATH=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.Droid/bin/Release/uno.platform.uitestsample.apk
+export UNO_UITEST_ANDROIDAPK_PATH=$BUILD_SOURCESDIRECTORY/src/Sample/Sample/bin/Release/net8.0-android/com.companyname.sample-Signed.apk
 export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/Sample.UITests.csproj
 export UNO_UITEST_ANDROID_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample/Sample.csproj
 export UNO_UITEST_ANDROID_API_LEVEL=28
