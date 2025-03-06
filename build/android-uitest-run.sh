46c277893d89a4ea2f69bfc503535e42a61fe91a
Jerome Laban <jerome@platform.uno>
2025-03-06 08:24:16 -0500

    chore: Adjust android project path

--- build/android-uitest-run.sh
+++ build/android-uitest-run.sh
@@ -4,7 +4,7 @@
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/android
 export UNO_UITEST_ANDROIDAPK_PATH=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.Droid/bin/Release/uno.platform.uitestsample.apk
 export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/Sample.UITests.csproj
-export UNO_UITEST_ANDROID_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.Droid/Sample.Droid.csproj
+export UNO_UITEST_ANDROID_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample/Sample.csproj
 export UNO_UITEST_ANDROID_API_LEVEL=28
 
 cd $BUILD_SOURCESDIRECTORY/build
