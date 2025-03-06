854b0a671c8f342c2b3f90b7e6132f72ccc72eb1
Jerome Laban <jerome@platform.uno>
2025-03-06 07:53:07 -0500

    chore: Adjust android and ios builds

--- build/scripts/ios-uitest.sh
+++ build/scripts/ios-uitest.sh
@@ -7,7 +7,6 @@ xcrun simctl list devices --json
 
 /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator &
 
-msbuild /r /p:Configuration=Release $UNO_UITEST_PROJECT
 dotnet build -c Release -f net8.0-ios -p:TargetFrameworks=net8.0-ios /p:IsUiAutomationMappingEnabled=True $UNO_UITEST_IOS_PROJECT
 
 mkdir -p $UNO_UITEST_SCREENSHOT_PATH
