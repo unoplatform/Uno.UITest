5759b34aeb29a9c67e98ba0d5057eb03947b691b
Jerome Laban <jerome@platform.uno>
2025-03-06 08:43:01 -0500

    chore: Adjust result files

--- build/android-uitest-run.sh
+++ build/android-uitest-run.sh
@@ -6,6 +6,7 @@ export UNO_UITEST_ANDROIDAPK_PATH=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.Droi
 export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample.UITests/Sample.UITests.csproj
 export UNO_UITEST_ANDROID_PROJECT=$BUILD_SOURCESDIRECTORY/src/Sample/Sample/Sample.csproj
 export UNO_UITEST_ANDROID_API_LEVEL=28
+export UNO_TEST_RESULTS_FILE=$BUILD_ARTIFACTSTAGINGDIRECTORY/test-results/uitest-android.xml
 
 cd $BUILD_SOURCESDIRECTORY/build
 scripts/android-uitest.sh
