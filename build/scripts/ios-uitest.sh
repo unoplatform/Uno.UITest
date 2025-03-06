07f4534fa5b053249da4dc1fa11b5838acfab0c2
Jerome Laban <jerome@platform.uno>
2025-03-06 13:58:36 -0500

    chore: Terminate background test processes

--- build/scripts/ios-uitest.sh
+++ build/scripts/ios-uitest.sh
@@ -37,4 +37,7 @@ mkdir -p $UNO_UITEST_SCREENSHOT_PATH
 
 dotnet test -c Release \
 	$UNO_UITEST_PROJECT \
-	--logger "nunit;LogFileName=$UNO_TEST_RESULTS_FILE"
+	--logger "nunit;LogFileName=$UNO_TEST_RESULTS_FILE" || true
+
+# terminate the bg task
+kill %1
